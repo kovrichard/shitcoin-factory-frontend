ff9bf811eeb9ca64011559d49bbf99a408ba7d62
Richard Kovacs <richardkovacs.it@gmail.com>
2022-01-23 16:40:22 +0100

    feat(build): add types to tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Web3ModalService } from '@mindsorg/web3modal-angular';
 
 import { HomeComponent } from './home.component';
 
@@ -9,6 +10,7 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomeComponent],
+      providers: [Web3ModalService],
     }).compileComponents();
   });
 
