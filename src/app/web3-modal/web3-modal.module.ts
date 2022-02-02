90071f497e2fe6de82ecfe7a3d06a575145d6323
Richard Kovacs <richardkovacs.it@gmail.com>
2022-02-02 20:44:05 +0100

    refactor(modal): remove modal completely

--- src/app/web3-modal/web3-modal.module.ts
+++ src/app/web3-modal/web3-modal.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Web3ModalComponent } from './web3-modal.component';
 import { Web3ModalMetamaskLogoComponent } from './web3-modal-metamask-logo.component';
-import { Modal } from '../modal/modal.component';
 
 @NgModule({
-  declarations: [Web3ModalComponent, Web3ModalMetamaskLogoComponent, Modal],
+  declarations: [Web3ModalComponent, Web3ModalMetamaskLogoComponent],
   imports: [CommonModule],
   exports: [Web3ModalComponent],
 })
