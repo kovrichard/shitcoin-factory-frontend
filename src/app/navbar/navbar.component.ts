c980159e248a52cce46f2faccee06eafc9e7b073
Richard Kovacs <richardkovacs.it@gmail.com>
2022-02-05 20:33:03 +0100

    feat(web3): load contract abi

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import Web3 from 'web3';
 import { Web3ModalService } from '../web3-modal/web3-modal.service';
 import { provider } from 'web3-core';
+import abi from '../web3-modal/factory-abi.json';
+import { AbiItem } from 'web3-utils';
 
 @Component({
   selector: 'app-navbar',
@@ -12,13 +14,16 @@ export class NavbarComponent implements OnInit {
   private web3: Web3;
   connected: boolean;
   account: string | null;
+  factoryAbi: AbiItem[];
+  contractAddress: string;
 
   constructor(private web3modalService: Web3ModalService) {
     this.web3 = new Web3();
     this.account = '';
+    this.factoryAbi = abi as AbiItem[];
   }
 
-  ngOnInit(): void {}
+  ngOnInit() {}
 
   async connect() {
     const provider = await this.web3modalService.open();
