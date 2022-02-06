2541bb15fe4ba445ab006cf1556425dc0de1b575
Richard Kovacs <richardkovacs.it@gmail.com>
2022-02-06 16:14:04 +0100

    refactor(modal): use metamask svg instead of component

--- src/app/web3-modal/web3-modal.module.ts
+++ src/app/web3-modal/web3-modal.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Web3ModalComponent } from './web3-modal.component';
-import { Web3ModalMetamaskLogoComponent } from './web3-modal-metamask-logo.component';
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [Web3ModalComponent, Web3ModalMetamaskLogoComponent],
+  declarations: [Web3ModalComponent],
   imports: [CommonModule, MatButtonModule],
   exports: [Web3ModalComponent],
 })
