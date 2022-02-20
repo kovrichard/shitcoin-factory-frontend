4f5d202c82881e76c5d4d1d84a4e3655dfa55195
Richard Kovacs <richardkovacs.it@gmail.com>
2022-02-20 23:55:57 +0100

    feat(navbar): simplify code

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Web3ModalService } from '../web3-modal/web3-modal.service';
 import abi from '../web3-modal/factory-abi.json';
 import { AbiItem } from 'web3-utils';
+import { ShitcoinFactoryService } from '../shitcoin-factory.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,38 +10,12 @@ import { AbiItem } from 'web3-utils';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  connected: boolean;
-  account: string | null = '';
-  factoryAbi = abi as AbiItem[];
-  contractAddress = '0x690d9f481E65Ab709Ba96DeFe20993A146a05f6c';
 
-  constructor(private web3modalService: Web3ModalService) {}
+  constructor(private shitcoinFactory: ShitcoinFactoryService) {}
 
   ngOnInit() {}
 
-  log() {
-    this.web3modalService.web3.eth
-      .getAccounts()
-      .then((accounts) => {
-        this.account = accounts[0];
-      })
-      .catch((error) => {
-        this.account = null;
-      });
-  }
-
   async mint() {
-    console.log(this.factoryAbi);
-    const factory = new this.web3modalService.web3.eth.Contract(
-      this.factoryAbi,
-      this.contractAddress
-    );
-    const numCoins = await factory.methods.numberOfCoins().call();
-    console.log(numCoins);
-    factory.methods
-      .create('Manon Coin', 'MANON', 1)
-      .send({ from: this.account });
-    const coin = await factory.methods.getShitcoin(numCoins - 1).call();
-    console.log(coin);
+    this.shitcoinFactory.create('Manon Coin', 'MANON', 1);
   }
 }
