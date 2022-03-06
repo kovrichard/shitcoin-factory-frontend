3a4019ef320e9e6c6d4efbef931cac63c1ea1a60
Richard Kovacs <richardkovacs.it@gmail.com>
2022-03-06 17:51:14 +0100

    refactor(web3-modal): simplify code

--- src/app/web3-modal/web3-modal.service.ts
+++ src/app/web3-modal/web3-modal.service.ts
@@ -6,21 +6,17 @@ import { ProviderController } from '../providers.service';
 
 @Injectable()
 export class Web3ModalService {
-  private providerController: ProviderController;
+  private providerController = new ProviderController(['metamask']);
 
   p: ethers.providers.Web3Provider | ethers.providers.JsonRpcProvider;
-  ps = new BehaviorSubject('' as any);
+  ps = new BehaviorSubject(this.providerController.providers);
   signer: ethers.providers.JsonRpcSigner;
   account = new BehaviorSubject('');
   s = new BehaviorSubject<ethers.providers.JsonRpcSigner>('' as any);
 
   constructor() {
-    this.providerController = new ProviderController(['metamask']);
-    this.providerController.providers;
     this.p = new ethers.providers.JsonRpcProvider('http://localhost:8545');
-    this.account.next('');
     this.signer = this.p.getSigner();
-    this.ps.next(this.providerController.providers);
   }
 
   async loadProviders() {
