7003b34995b0971ea89eb10aa7d567df3432ccf0
Richard Kovacs <richardkovacs.it@gmail.com>
2022-03-06 13:48:06 +0100

    feat(modal-service): add missing tests

--- src/app/web3-modal/web3-modal.service.spec.ts
+++ src/app/web3-modal/web3-modal.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { JsonRpcProvider } from '@ethersproject/providers';
 import { generateTestingUtils } from 'eth-testing';
+import { ethers } from 'ethers';
+import { IProvider } from '../providers';
+import { ProviderController } from '../providers.service';
 import { Web3ModalService } from './web3-modal.service';
+
+const fakeProviderController = {
+  connected: {
+    subscribe: ((cb: any) => {
+      console.log('no')
+      cb('what');
+    })
+  }
+}
 
 describe('Web3ModalService', () => {
   let service: Web3ModalService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      providers: [Web3ModalService],
+      providers: [Web3ModalService, {
+        provide: ProviderController,
+        useValue: fakeProviderController
+      }],
     }).compileComponents();
 
     const testingUtils = generateTestingUtils({ providerType: 'MetaMask' });
+    testingUtils.mockAccounts(['0x138071e4e810f34265bd833be9c5dd96f01bd8a5']);
+    testingUtils.mockRequestAccounts(['0x138071e4e810f34265bd833be9c5dd96f01bd8a5']);
     (window as any).ethereum = testingUtils.getProvider();
   });
 
@@ -18,8 +36,36 @@ describe('Web3ModalService', () => {
     service = TestBed.inject(Web3ModalService);
   });
 
+  it('should set default values', () => {
+    expect(service.p).toBeInstanceOf(ethers.providers.JsonRpcProvider);
+    service.account.subscribe((value: string) => {
+      expect(value).toEqual('');
+    });
+    expect(service.signer).toBeInstanceOf(ethers.providers.JsonRpcSigner);
+    service.ps.subscribe((value: IProvider[]) => {
+      expect(value[0].name).toEqual('MetaMask');
+    });
+  });
+
   it('loadProviders should initialize providers', fakeAsync(() => {
     service.loadProviders();
     tick();
+
+    expect(service.p).toBeInstanceOf(ethers.providers.Web3Provider);
+    expect(service.signer).toBeInstanceOf(ethers.providers.JsonRpcSigner);
+    service.s.subscribe((value: ethers.providers.JsonRpcSigner) => {
+      expect(value).toEqual(service.signer);
+    });
+    service.account.subscribe((value: string) => {
+      expect(value.toLowerCase()).toEqual('0x138071e4e810f34265bd833be9c5dd96f01bd8a5');
+    });
+  }));
+
+  xit('open should work', fakeAsync(() => {
+    const subscribeMock = spyOn(fakeProviderController.connected, 'subscribe');
+    service.open();
+    tick();
+
+    expect(subscribeMock).toHaveBeenCalledOnceWith('');
   }));
 });
