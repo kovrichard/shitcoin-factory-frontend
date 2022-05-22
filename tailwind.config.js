aa6b20b234b6abc97b019b7f488187fb9299b950
Richard Kovacs <richardkovacs.it@gmail.com>
2022-05-22 12:12:39 +0200

    feat(style): add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+    colors: {
+      'primary': '#167ffc',
+      'accent': '#7579ff',
+      'warn': '#fd3259'
+    }
+  },
+  plugins: [],
+}
