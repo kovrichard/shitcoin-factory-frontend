7dae492a567d71b3a8e7defe2be5b6cb7264ca47
Richard Kovacs <richardkovacs.it@gmail.com>
2022-05-23 10:04:00 +0200

    style: add full mobile support to navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,18 +1,16 @@
 import {
-  AfterViewInit,
   Component,
   ElementRef,
   Input,
   OnInit,
   ViewChild,
 } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit, AfterViewInit {
+export class NavbarComponent implements OnInit {
   @Input() home: HTMLDivElement;
   @Input() about: HTMLDivElement;
   @Input() factory: HTMLDivElement;
@@ -23,27 +21,9 @@ export class NavbarComponent implements OnInit, AfterViewInit {
 
   darkMode = false;
 
-  constructor(private breakpoints: BreakpointObserver) {}
+  constructor() {}
 
   ngOnInit() {}
-
-  ngAfterViewInit() {
-    this.breakpoints.observe(Breakpoints.XSmall).subscribe((result: any) => {
-      const menuButton = document.getElementsByClassName('menu')[0];
-
-      if (result.matches) {
-        this.navbar.nativeElement.classList.remove('d-flex');
-        this.navbar.nativeElement.classList.add('d-none');
-        menuButton.classList.remove('d-none');
-        menuButton.classList.add('d-flex');
-      } else {
-        this.navbar.nativeElement.classList.remove('d-none');
-        this.navbar.nativeElement.classList.add('d-flex');
-        menuButton.classList.remove('d-flex');
-        menuButton.classList.add('d-none');
-      }
-    });
-  }
 
   scroll($element: HTMLDivElement) {
     $element.scrollIntoView({
