274b90b68d8c1b62717937bab2c9f22e5dec72f4
Richard Kovacs <richardkovacs.it@gmail.com>
2022-07-06 08:34:21 +0200

    feat(env): add missing env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+  contractAddress: '0x0000000000000000000000000000000000000000',
   bscShapeshifterAddress: '0x0000000000000000000000000000000000000000',
   ethShapeshifterAddress: '0x0000000000000000000000000000000000000000',
+  networkUrl: 'https://bsc-dataseed.binance.org/',
 };
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+  contractAddress: '0x0000000000000000000000000000000000000000',
   bscShapeshifterAddress: '0x10624C0Da18A4DB7E48069CDA17398a5028F93Bd',
   ethShapeshifterAddress: '0x9ED1d5e3C10a19A7Ce5cC1C9Da17B77E9Eef0eE1',
+  networkUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
 };
