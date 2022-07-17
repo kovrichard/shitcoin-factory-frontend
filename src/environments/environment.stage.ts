347a2d8a15ad84b53236b7afbba1570b8d203466
Richard Kovacs <richardkovacs.it@gmail.com>
2022-07-17 14:19:05 +0200

    feat(testnet): add testnet url

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  contractAddress: '0x0000000000000000000000000000000000000000',
+  contractAddress: '0xF24B94Ea9Ea201794622aF0DdAEa1f9d7Cf41c05',
   bscShapeshifterAddress: '0x10624C0Da18A4DB7E48069CDA17398a5028F93Bd',
   ethShapeshifterAddress: '0x9ED1d5e3C10a19A7Ce5cC1C9Da17B77E9Eef0eE1',
   networkUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
