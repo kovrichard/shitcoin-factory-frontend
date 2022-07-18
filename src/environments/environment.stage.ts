d14b4128ec69690bc2cae14654ba269b18341abc
Richard Kovacs <richardkovacs.it@gmail.com>
2022-07-18 20:21:42 +0200

    feat(env): add new contract address

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  contractAddress: '0xF24B94Ea9Ea201794622aF0DdAEa1f9d7Cf41c05',
+  contractAddress: '0xdeA0F97BA80d0E41cb2063eE65CF7A9fB6D4f386',
   bscShapeshifterAddress: '0x10624C0Da18A4DB7E48069CDA17398a5028F93Bd',
   ethShapeshifterAddress: '0x9ED1d5e3C10a19A7Ce5cC1C9Da17B77E9Eef0eE1',
   networkUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
