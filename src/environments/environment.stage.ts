0f320cf7e77be284e1bcaba05d7aad93628a4672
Richard Kovacs <richardkovacs.it@gmail.com>
2022-10-07 23:37:10 +0200

    feat: change addresses to sepolia from ropsten

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-  etherScan: 'https://ropsten.etherscan.io',
+  etherScan: 'https://sepolia.etherscan.io',
   bscScan: 'https://testnet.bscscan.com',
   polygonScan: 'https://mumbai.polygonscan.com',
   etherContractAddress: '0x1f470e946942AAa6406bAd8Cd6AFdcA7020B3DB7',
   bscContractAddress: '0xcEb0D98c8278Ba76e81dc82ea5BeE1679Dfa6Bbd',
   polygonContractAddress: '0xFE757CDb6093dF085766c424F69Eec80Cc50babE',
   etherNetworkUrl:
-    'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    'https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
   bscNetworkUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545',
   polygonNetworkUrl:
     'https://polygon-mumbai.g.alchemy.com/v2/of7zk4E0TWkV6pGWOPJU1_S_9cdjTpwY',
