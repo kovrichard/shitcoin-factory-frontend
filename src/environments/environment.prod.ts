50088eb0eda30d2dabe9a58c079701639bb5cd2e
Richard Kovacs <richardkovacs.it@gmail.com>
2022-10-10 21:36:00 +0200

    feat: add cronos mainnet deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ export const environment = {
   bscContractAddress: '0x20e18C8581f3FE4C1A03Fe3711a5BA74EC40808C',
   polygonContractAddress: '0x1f470e946942AAa6406bAd8Cd6AFdcA7020B3DB7',
   avaxContractAddress: '0x1f470e946942AAa6406bAd8Cd6AFdcA7020B3DB7',
-  cronosContractAddress: '0x0000000000000000000000000000000000000000',
+  cronosContractAddress: '0xa9010B50F8664Aa95B201021B5f0a21191339890',
   etherNetworkUrl:
     'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
   bscNetworkUrl: 'https://bsc-dataseed.binance.org/',
@@ -17,5 +17,5 @@ export const environment = {
     'https://polygon-mainnet.g.alchemy.com/v2/KFg5XRbhWACnSVqk9_XSNsEf4aD093AU',
   avaxNetworkUrl: 'https://api.avax.network/ext/bc/C/rpc',
   cronosNetworkUrl: 'https://evm.cronos.org',
-  validChains: [1, 56, 137, 43114],
+  validChains: [1, 25, 56, 137, 43114],
 };
